15908aebb16a34317d756fa53f0ff3b45e504acb
AaronFeng1 <fenga3684@gmail.com>
2025-06-05 12:03:46 -0700

    Reduce the lines of code and finish the project

--- calc.js
+++ calc.js
@@ -1,7 +1,29 @@
+const output = document.querySelector(".output");
+const btn9 = document.querySelector(".nine");
+const btn8 = document.querySelector(".eight");
+const btn7 = document.querySelector(".seven");
+const btn6 = document.querySelector(".six");
+const btn5 = document.querySelector(".five");
+const btn4 = document.querySelector(".four");
+const btn3 = document.querySelector(".three");
+const btn2 = document.querySelector(".two");
+const btn1 = document.querySelector(".one");
+const btn0 = document.querySelector(".zero");
+
+const btnAdd = document.querySelector(".add");
+const btnSub = document.querySelector(".sub");
+const btnMult = document.querySelector(".mult");
+const btnDivdie = document.querySelector(".divide");
+const clear = document.querySelector(".spec");
+const equal = document.querySelector(".equal");
+
+let arrNums = [btn9, btn8, btn7, btn6, btn5, btn4, btn3, btn2, btn1, btn0];
+let arrOps = [btnAdd, btnSub, btnMult, btnDivdie];
+
 let clicked = false;
 let counter = 0;
 let display = "";
-let checkForNewResult = false;
+let checkIfNewCalculation = false;
 
 function add(a, b){
     return a + b;
@@ -32,237 +54,16 @@ let operator = ""
 function operate(num1, num2, operator){
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
-    if(operator === "add"){
+    if(operator === "+"){
         return add(num1, num2);
-    } else if(operator === "sub"){
+    } else if(operator === "-"){
         return sub(num1, num2);
-    } else if(operator === "mult"){
+    } else if(operator === "x"){
         return mult(num1, num2);
-    } else if(operator === "divide"){
+    } else if(operator === "%"){
         return divide(num1, num2);
     }
 }
-
-const output = document.querySelector(".output");
-const btn9 = document.querySelector(".nine");
-const btn8 = document.querySelector(".eight");
-const btn7 = document.querySelector(".seven");
-const btn6 = document.querySelector(".six");
-const btn5 = document.querySelector(".five");
-const btn4 = document.querySelector(".four");
-const btn3 = document.querySelector(".three");
-const btn2 = document.querySelector(".two");
-const btn1 = document.querySelector(".one");
-const btn0 = document.querySelector(".zero");
-
-btn9.addEventListener("click", function(){
-    
-    if(clicked === true){
-        num2 += "9";
-        display += "9";
-        return output.textContent = display;
-    } else if(checkForNewResult = true){
-        num1 = "";
-        display = "";
-        num1 +=  "9";
-        display += "9";
-        return output.textContent = display
-    } else {
-        num1 +=  "9";
-        display += "9";
-        return output.textContent = display;
-    }
-});
-
-btn8.addEventListener("click", function(){
-    if(clicked === true){
-        num2 += "8";
-        display += "8";
-        return output.textContent = display;
-    } else {
-        num1 +=  "8";
-        display += "8";
-        return output.textContent = display;
-    }
-});
-
-btn7.addEventListener("click", function(){
-    if(clicked === true){
-        num2 += "7";
-        display += "7";
-        return output.textContent = display; 
-    } else {
-        num1 +=  "7";
-        display += "7";
-        return output.textContent = display;
-    }
-});
-
-btn6.addEventListener("click", function(){
-    if(clicked === true){
-        num2 += "6";
-        display += "6";
-        return output.textContent = num2;
-    } else {
-        num1 +=  "6";
-        display += "6";
-        return output.textContent = display;
-    }
-});
-
-btn5.addEventListener("click", function(){
-    if(clicked === true){
-        num2 += "5";
-        display += "5";
-        return output.textContent = display;
-    } else {
-        num1 +=  "5";
-        display += "5";
-        return output.textContent = display;
-    }
-});
-
-btn4.addEventListener("click", function(){
-    if(clicked === true){
-        num2 += "4";
-        display += "4";
-        return output.textContent = display;
-    } else {
-        num1 +=  "4";
-        display += "4";
-        return output.textContent = display;
-    }
-});
-
-btn3.addEventListener("click", function(){
-    if(clicked === true){
-        num2 += "3";
-        display += "3";
-        return output.textContent = display;
-    } else {
-        num1 +=  "3";
-        display += "3";
-        return output.textContent = display;
-    }
-});
-
-btn2.addEventListener("click", function(){
-    if(clicked === true){
-        num2 += "2";
-        display += "2";
-        return output.textContent = display;
-    } else {
-        num1 +=  "2";
-        display += "2";
-        return output.textContent = display;
-    }
-});
-
-btn1.addEventListener("click", function(){
-    if(clicked === true){
-        num2 += "1";
-        display += "1";
-        return output.textContent = display;
-    } else {
-        num1 +=  "1";
-        display += "1";
-        return output.textContent = display
-    }
-});
-
-btn0.addEventListener("click", function(){
-    if(clicked === true){
-        num2 += "0";
-        display += "0";
-        output.textContent = display;
-    } else {
-        num1 += "0";
-        display += "0";
-        output.textContent = display;
-    }
-})
-const btnAdd = document.querySelector(".add");
-const btnSub = document.querySelector(".sub");
-const btnMult = document.querySelector(".mult");
-const btnDivdie = document.querySelector(".divide");
-
-let result = 0;
-
-btnAdd.addEventListener("click", function(){
-    counter++;
-    clicked = true;
-    display += " + ";
-    if(num2 === ""){
-        operator = "add";
-        output.textContent = display;
-    } else {
-        num1 = operate(num1, num2, operator);
-        num2 = "";
-        operator = "add";
-        display = num1 + " + ";
-        return output.textContent = display;
-    }
-})
-
-
-
-btnSub.addEventListener("click", function(){
-    counter++;
-    clicked = true;
-    display += " - ";
-    if(num2 === ""){
-        operator = "sub";
-        output.textContent = display;
-        counter = 1;
-    }
-    if(counter > 1){
-        num1 = operate(num1, num2, operator);
-        num2 = "";
-        operator = "sub";
-        display = num1 + " - ";
-        return output.textContent = display;
-    }
-})
-
-btnMult.addEventListener("click", function(){
-    counter++;
-    clicked = true;
-    display += " x ";
-    if(num2 === ""){
-        operator = "mult";
-        output.textContent = display;
-        counter = 1;
-    }
-    if(counter > 1){
-        num1 = operate(num1, num2, operator);
-        num2 = "";
-        operator = "mult";
-        display = num1 + " x ";
-        return output.textContent = display
-    }
-})
-
-btnDivdie.addEventListener("click", function(){
-    counter++;
-    clicked = true;
-    display += " % ";
-    if(num2 === ""){
-        operator = "divide";
-        output.textContent = display;
-        counter = 1;
-    }
-    if(counter > 1){
-        num1 = operate(num1, num2, operator);
-        num2 = "";
-        operator = "divide";
-        display = num1 + " % ";
-        return output.textContent = display;
-    }
-    operator = "divide";
-    output.textContent = display;
-})
-
-const clear = document.querySelector(".spec");
 
 clear.addEventListener("click", function(){
     num1 = "";
@@ -274,8 +75,6 @@ clear.addEventListener("click", function(){
     output.textContent = "";
 })
 
-const equal = document.querySelector(".equal");
-
 equal.addEventListener("click", function(){
     num1 = operate(num1, num2, operator);
     clicked = false;
@@ -283,6 +82,53 @@ equal.addEventListener("click", function(){
     display += num1;
     counter = 0;
     num2 = "";
-    checkForNewResult = true;
+    checkIfNewCalculation = true;
     output.textContent = num1;
+})
+
+arrNums.forEach(element => {
+    element.addEventListener("click", function(){
+        if(checkIfNewCalculation === true){
+            num1 = element.textContent;
+            num2 = "";
+            operator = "";
+            clicked = false;
+            display = num1;
+            checkIfNewCalculation = false
+            return output.textContent = display;
+        }
+        if(clicked === true){
+            checkIfNewCalculation = false;
+            num2 += element.textContent;
+            display += element.textContent;
+            output.textContent = display;
+        } else {
+            checkIfNewCalculation = false;
+            num1 += element.textContent;
+            display += element.textContent
+            output.textContent = display; 
+        }
+    });    
+});
+
+arrOps.forEach(btnOperator =>{
+    btnOperator.addEventListener("click", function(){
+        counter++;
+        clicked = true;
+        display += " " + btnOperator.textContent + " ";
+        if(num2 === ""){
+            checkIfNewCalculation = false;
+            operator = btnOperator.textContent;
+            output.textContent = display;
+            counter = 1;
+        }
+        if(counter > 1){
+            checkIfNewCalculation = false;
+            num1 = operate(num1, num2, operator);
+            num2 = "";
+            operator = btnOperator.textContent;
+            display = num1 + " " + btnOperator.textContent + " ";
+            return output.textContent = display;
+        }
+    })
 })
